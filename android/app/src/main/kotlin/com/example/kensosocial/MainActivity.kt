0f0759ff1c764b55c9f2857069d8f9132fad69d3
PhamQuocDuy <duy.187pm13901@vanlanguni.vn>
2022-03-28 18:42:06 +0700

    Update new code

---
+++ android/app/src/main/kotlin/com/example/kensosocial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kensosocial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Kenso',
+      debugShowCheckedModeBanner: false,
+      home: null,
+    );
+  }
+}
